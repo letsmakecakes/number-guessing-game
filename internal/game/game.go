729a95a1e6dd1fb3186bd1b7e4a92d87381af707
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-28 23:20:16 +0530

    added loop method to run the game in loop and some changes

--- internal/game/game.go
+++ internal/game/game.go
@@ -2,35 +2,89 @@ package game
 
 import (
 	"fmt"
+	"math/rand"
 	"number-guessing-game/pkg/utils"
 )
 
+type Diffculty struct {
+	Name    string
+	Chances int
+}
+
 func Start() {
-	printWelcomeMessage()
-	chances, err := getChances()
-	if err != nil {
-		fmt.Errorf("error getting chances: %v", err)
-	}
+	diff := getDifficulty()
+
+	target := rand.Intn(100) + 1
+
+	loop(diff.Chances, target)
 }
 
-func printWelcomeMessage() {
-	fmt.Println("Welcome to the Number Guessing Game!")
-	fmt.Println("I'm thinking of a number between 1 and 100.")
-	fmt.Println("\nPlease select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)")
+func getDifficulty() *Diffculty {
+	var diff Diffculty
+	for {
+		prompt := "Enter your choice: "
+		input, err := utils.ReadInput(prompt)
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue
+		}
+
+		choice, err := utils.ParseInt(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter 1, 2, or 3.")
+			continue
+		}
+
+		switch choice {
+		case 1:
+			diff = Diffculty{Name: "Easy", Chances: 10}
+		case 2:
+			diff = Diffculty{Name: "Medium", Chances: 5}
+		case 3:
+			diff = Diffculty{Name: "Hard", Chances: 3}
+		default:
+			fmt.Println("Invalid choice. Please enter 1, 2, or 3.")
+			continue
+		}
+		break
+	}
+
+	fmt.Printf("\nGreat! You have selected the %s difficulty level.\n", diff.Name)
+	fmt.Println("Let's start the game!")
+
+	return &diff
 }
 
-func getChances() (int, error) {
-	prompt := "Enter your choice: "
-	input, err := utils.ReadInput(prompt)
-	if err != nil {
-		return 0, fmt.Errorf("error reading input: %v", err)
+func loop(chances int, target int) {
+	attempts := 0
+	for attempts < chances {
+		fmt.Printf("Attempt %d/%d\n", attempts+1, chances)
+		prompt := "Enter your guess: "
+		input, err := utils.ReadInput(prompt)
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue
+		}
 
+		guess, err := utils.ParseInt(input)
+		if err != nil || guess < 1 || guess > 100 {
+			fmt.Println("Invalid guess. Please enter a number between 1 and 100.")
+			continue
+		}
+
+		attempts++
+
+		if guess == target {
+			fmt.Printf("Congragulations! You guess the correct number in %d attempts.\n", attempts)
+			return
+		} else if guess < target {
+			fmt.Println("Incorret! The number is greater than", guess)
+		} else {
+			fmt.Println("Incorret! The number is less than", guess)
+		}
+
+		fmt.Println()
 	}
 
-	chances, err := utils.ParseInt(input)
-	if err != nil {
-		return 0, fmt.Errorf("error parsing int: %v", err)
-	}
-
-	return chances, nil
+	fmt.Printf("Sorry, you've run out of chances. The correct number was %d.\n", target)
 }
